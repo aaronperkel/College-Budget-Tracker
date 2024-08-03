7985c4b32117f658950348ead59976b163ab656b
aaronperkel <aaron.perkel27@gmail.com>
2024-08-02 23:40:12 -0400

    fixed typo in footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
         <footer>
             <p>
-                529 Tracker. Created by Aaron Perkel in July 2024.
+                529 Tracker. Created by Aaron Perkel in August 2024.
             </p>
         </footer>   
     </body>
